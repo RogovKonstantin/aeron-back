4e833ba2113633518b46f744f854fbce81f7e1a1
d50776310@gmail.com <d50776310@gmail.com>
2024-07-06 16:22:38 +0300

    c

--- app.py
+++ app.py
@@ -1,133 +1,113 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import psycopg2
+from psycopg2 import sql
 
 DB_NAME = 'acars'
 DB_USER = 'user_back'
 DB_PASSWORD = 'Gdfhg354'
+DB_HOST = "127.0.0.1"
+DB_PORT = "5432"
 
 app = Flask(__name__)
 CORS(app, resources={r"/download": {"origins": "*"}})
 
 
 def get_db_connection():
-    return psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host="127.0.0.1", port="5432")
+    return psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
 
 
-def save_message_to_database(message, result):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    query = "INSERT INTO message (message, result) VALUES (%s, %s)"
-    cursor.execute(query, (message, result))
-    conn.commit()
-    conn.close()
+def execute_query(query, params=None, fetchone=False, fetchall=False):
+    result = None
+    with get_db_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(query, params)
+            if fetchone:
+                result = cursor.fetchone()
+            if fetchall:
+                result = cursor.fetchall()
+    return result
 
 
-def save_template_to_database(template_name, picking_template, template, model, message, folder_id):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    query = "INSERT INTO templates (template_name, picking_template, template, model, message, folder_id) VALUES (%s, %s, %s, %s, %s, %s)"
-    cursor.execute(query, (template_name, picking_template, template, model, message, folder_id))
-    conn.commit()
-    conn.close()
-
-
-def save_folder_to_database(folder_name, parent_id):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO folders (folder_name, parent_id) VALUES (%s, %s)',
-                   (folder_name, parent_id))
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-
-def get_template_from_database(template_id):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    query = "SELECT template FROM templates WHERE template_id = %s"
-    cursor.execute(query, (template_id,))
-    template = cursor.fetchone()
-    conn.close()
-    return template
-
-
-def get_all_templates_from_database():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    query = "SELECT template_id, template_name, folder_id FROM templates"
-    cursor.execute(query)
-    templates = cursor.fetchall()
-    conn.close()
-    return templates
+def save_to_database(table, columns, values):
+    query = sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
+        sql.Identifier(table),
+        sql.SQL(', ').join(map(sql.Identifier, columns)),
+        sql.SQL(', ').join(sql.Placeholder() * len(values))
+    )
+    execute_query(query, values)
 
 
 @app.route("/save-message", methods=["POST"])
 def save_message():
     data = request.get_json()
-    if data:
-        message = data.get("message")
-        result = data.get("result")
-        if message and result:
-            save_message_to_database(message, result)
-            return "Message saved to database", 201
-        else:
-            return "Incomplete data", 400
-    else:
+    if not data:
         return "No data received", 400
+
+    message = data.get("message")
+    result = data.get("result")
+    if not (message and result):
+        return "Incomplete data", 400
+
+    save_to_database('message', ['message', 'result'], [message, result])
+    return "Message saved to database", 201
 
 
 @app.route("/save-template", methods=["POST"])
 def save_template():
     data = request.get_json()
-    if data:
-        template_name = data.get("template_name")
-        picking_template = data.get("picking_template")
-        template = data.get("template")
-        model = data.get("model")
-        message = data.get("message")
-        folder_id = data.get("folder_id")
-        if template_name and picking_template and template and model and message and folder_id:
-            save_template_to_database(template_name, picking_template, template, model, message, folder_id)
-            return "Template saved to database", 201
-        else:
-            return "Incomplete data", 400
-    else:
+    if not data:
         return "No data received", 400
+
+    required_fields = ['template_name', 'picking_template', 'template', 'model', 'message', 'folder_id']
+    if not all(field in data for field in required_fields):
+        return "Incomplete data", 400
+
+    save_to_database('templates', required_fields, [data[field] for field in required_fields])
+    return "Template saved to database", 201
 
 
 @app.route("/save-folder", methods=["POST"])
 def save_folder():
     data = request.get_json()
-    if data:
-        folder_name = data.get("folder_name")
-        parent_id = data.get("parent_id")
-        if folder_name and (parent_id is None or isinstance(parent_id, int)):
-            save_folder_to_database(folder_name, parent_id)
-            return "Folder saved to database", 201
-        else:
-            return "Incomplete data", 400
-    else:
+    if not data:
         return "No data received", 400
+
+    folder_name = data.get("folder_name")
+    parent_id = data.get("parent_id")
+    if not folder_name or (parent_id is not None and not isinstance(parent_id, int)):
+        return "Incomplete data", 400
+
+    save_to_database('folders', ['folder_name', 'parent_id'], [folder_name, parent_id])
+    return "Folder saved to database", 201
 
 
 @app.route("/get-template/<int:template_id>", methods=["GET"])
 def get_template(template_id):
-    template = get_template_from_database(template_id)
+    query = "SELECT template FROM templates WHERE template_id = %s"
+    template = execute_query(query, (template_id,), fetchone=True)
     if template:
-        return jsonify({
-            "template": template[0],
-        })
-    else:
-        return "Template not found", 404
+        return jsonify({"template": template[0]})
+    return "Template not found", 404
 
 
 @app.route("/get-all-templates", methods=["GET"])
 def get_all_templates():
-    templates = get_all_templates_from_database()
+    query = "SELECT template_id, template_name, folder_id FROM templates"
+    templates = execute_query(query, fetchall=True)
     return jsonify([
-        {"template_id": template[0], "template_name": template[1],"folder_id": template[2]}
+        {"template_id": template[0], "template_name": template[1], "folder_id": template[2]}
         for template in templates
+    ])
+
+
+@app.route("/get-all-folders", methods=["GET"])
+def get_all_folders():
+    query = "SELECT * FROM folders"
+    folders = execute_query(query, fetchall=True)
+    return jsonify([
+        {"folder_id": folder[0], "folder_name": folder[1], "parent_id": folder[2]}
+        for folder in folders
     ])
 
 
